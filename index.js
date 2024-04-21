dc0c529321071a61e69add6f5b2680dc07fdfa31
Phumin Tongla <110321268+Natsu2022@users.noreply.github.com>
2024-04-21 20:34:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app  = express();
+const bodyParser = require('body-parser');
 const port = 3000;
 
+app.use(bodyParser.json());
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
 app.get('/', (req, res) => {
-    res.send('API is runing...');
+    res.json({ message: 'Hello World' });
 });
 
 app.get('/about', (req, res) => {
