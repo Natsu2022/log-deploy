69f2347967d5635e1d9cb8a22e0ce141139d0284
Phumin Tongla <110321268+Natsu2022@users.noreply.github.com>
2024-04-21 17:54:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,17 @@
-const express = require("express");
-const { MongoClient } = require("mongodb");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const cookieSession = require("cookie-session");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { body, validationResult } = require("express-validator")
+const express = require('express');
+const app  = express();
+const port = 3000;
 
-// Load environment variables
-require("dotenv").config();
-
-// Initialize Express app
-const app = express();
-const port = 8000; // Port number
-
-const uri = "mongodb+srv://Adminton:e2Z57dwLeO7F6AHV@cluster.3nvsqiu.mongodb.net/"; // Connection URI
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
 
-app.use(bodyParser.json());
-const corsOptions = process.env.CORSOPTION;
-app.use(cors(corsOptions));
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
 app.get('/', (req, res) => {
-    res.send('Server is working!');
+    res.send('API is working!');
 });
 
 app.get('/about', (req, res) => {
     res.send('About Us');
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
 });
 
 module.exports = app;
