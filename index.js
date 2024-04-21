1da8c564a7083787f16f0e4cc3b5e4e65d5301c7
Phumin Tongla <110321268+Natsu2022@users.noreply.github.com>
2024-04-21 21:01:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,18 +17,35 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
 
-async function connect() {
-        await cliecnt.connect();
-        console.log('Connected to MongoDB');
-
-        app.get('/reginter', async (req, res) => {
-            const logs = await cliecnt.db('logs').collection('log').find({}).toArray();
-            res.json(logs);
-        });
-}
-
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const db = client.db('UsersDB');
+    const collection = db.collection('Log');
+    const user = await collection.ficdOne({ username });
+    // If the user doesn't exist, return an error
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      // Ensure that the user's hashed password is not undefined
+      if (!user.Password) {
+        return res.status(500).json({ message: "Invalid user data" });
+      }
+    
+    const isPasswordValid = await bcrypt.compare(password, user.Password);
+
+    // If passwords match, set session data and return success message
+    if (isPasswordValid) {
+        return res
+        .status(200)
+        .json({ message: "User logged in successfully" });
+    } else {
+        return res.status(401).json({ message: "Invalid password" });
+    }
 });
 
 app.get('/', (req, res) => {
