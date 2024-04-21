e2cc5b2c46ae76998baaff9420679b007e84342f
Phumin Tongla <110321268+Natsu2022@users.noreply.github.com>
2024-04-21 17:35:02 +0700

    update

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
-const express = require('express');
-const app  = express();
-const port = 3000;
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieSession = require("cookie-session");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { body, validationResult } = require("express-validator")
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+// Load environment variables
+require("dotenv").config();
+
+// Initialize Express app
+const app = express();
+const port = process.env.PORT; // Port number
+
+const uri = process.env.DB_URL; // Connection URI
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+const secret = "mysecret"; // Secret key for JWT
+// const token = jwt.sign({ username, role: 'admin' }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+app.use(bodyParser.json());
+const corsOptions = process.env.CORSOPTION;
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+const ifNotLoggedin = (req, res, next) => {
+  if (!req.session.isLoggedIn) {
+    return res.status(401).json({ message: "You are not logged in" });
+  }
+  next();
+};
+
+const ifLoggedin = (req, res, next) => {
+  if (req.session.isLoggedIn) {
+    return res.status(401).json({ message: "You are already logged in" });
+  }
+  next();
+};
 
 app.get('/', (req, res) => {
     res.send('API is working!');
