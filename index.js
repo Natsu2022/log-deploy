bfcac84e7134562994ccf547ad32109009f613df
Phumin Tongla <110321268+Natsu2022@users.noreply.github.com>
2024-04-21 20:51:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const app  = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
-const port = 3000;
+const { MongoClient } = require('mongodb');
 
+const port = 3000;
+const corsOptions = process.env.CORS_OPTIONS;
+
+const db_url = process.env.DB_URL;
+const cliecnt = new MongoClient(db_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+app.use(cors());
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
+
+async function connect() {
+    try {
+        await cliecnt.connect();
+        console.log('Connected to MongoDB');
+
+        app.get('/reginter', async (req, res) => {
+            const logs = await cliecnt.db('logs').collection('log').find({}).toArray();
+            res.json(logs);
+        });
+    } catch (error) {
+        console.error('Error connecting to MongoDB', error);
+    }
+}
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
