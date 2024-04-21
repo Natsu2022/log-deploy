6de0fd1a9a8350fcfed135d7355deafa4715ed80
Phumin Tongla <110321268+Natsu2022@users.noreply.github.com>
2024-04-21 16:32:37 +0700

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app  = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('API is working!');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Us');
+});
+
+module.exports = app;
